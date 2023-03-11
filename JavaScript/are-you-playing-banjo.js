d8adcd2472fb04948dc961d15195f2bae78341f3
Daniil Shatukhin <daniilshstukhin@gmail.com>
2023-03-11 22:14:49 +0400

    Add kata Terminal game move function by Grasshopper

---
+++ JavaScript/are-you-playing-banjo.js
@@ -0,0 +1,23 @@
+/*
+SOLUTION WORKS BUT KATA TESTS ARE BROKEN
+
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+```
+name + " plays banjo"
+name + " does not play banjo"
+```
+
+Names given are always valid strings.
+ */
+
+const areYouPlayingBanjo = (name) => {
+    if (name.toLowerCase().includes('r')) {
+        return `${name} plays banjo`;
+    }  else {
+        return `${name} does not play banjo`;
+    }
+}
